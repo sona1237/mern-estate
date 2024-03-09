e76308ff064946a57fcfc8fc80889e0e4f2b70aa
sonarajan <sonarajan456@gmail.com>
2024-03-09 08:36:37 +0530

    create an run

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express"
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb+srv://netninja:Sona123Test@cluster0.juhwuwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const app = express()
 
 app.listen(3000, () => {
-    console.log("server running on port 3000!!")
+    console.log("server running on port 3000!!")                                                                                 
 })
